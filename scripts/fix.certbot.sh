5394e75d5d523fdb9c0b199798645f7882ce611a
Piotr Krol <45247798+krolpiotr@users.noreply.github.com>
2018-11-22 06:22:13 +0100

    Create fix.certbot.sh

---
+++ scripts/fix.certbot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# fix.certbot.sh
+#
+# version 1.0.0
+#
+# Installing Certbot on your Ubuntu Desktop 16.04
+# author: Piotr Krol, simonphoenix.px@gmail.com
+
+# ------------------------------------------------------------------------
+# name: install_java() 
+# desc: Install
+# args: 
+# return: 0
+# ------------------------------------------------------------------------
+install_certbot() {
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install python-certbot-apache
+sudo certbot --apache -d zf3.simon-phoenix.se
+}
+# ------------------------------------------------------------------------
+
+echo '# -----------------------------------------------------------------------#'
+echo '#          Installing Certbot                                            #'
+echo '# -----------------------------------------------------------------------#'
+
+
+install_certbot
+
+
+echo '# -----------------------------------------------------------------------#'
+echo '#          Installing Certbot done.                                      #'
+echo '# -----------------------------------------------------------------------#'
