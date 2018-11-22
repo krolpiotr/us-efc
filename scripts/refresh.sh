02dd9b7eacc15bbc0dcab0d1166ce76e49899aca
krolpiotr <45247798+krolpiotr@users.noreply.github.com>
2018-11-22 03:38:17 +0100

    Update refresh.sh

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -2,6 +2,13 @@
 
 # refresh.sh
 
+# ------------------------------------------------------------------------ 
+# desc: update, upgrade and autoclean of system
+# 
+# author : Piotr Krol, simonphoenix.px@gmail.com
+# website: simon-phoenix.se
+# ------------------------------------------------------------------------
+
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get autoclean
 echo ""
